7e7b87b39c0552bc5aba1c302aa28b3f7ec0aaf9
Flavio Laino <info@flaviolaino.it>
2015-06-12 09:46:27 +0200

    Argument as object approach
    without Promise

--- example.js
+++ example.js
@@ -1,12 +1,11 @@
-ajax(
-	'',
-	'POST',
-	{ 'ciao': 'pippo', 'dd': { 'a': 'uno', 'b': 'due', 'c': 'tre' } }
-).then(
-	function(a){ // a = xmlhttp object
+ajax({
+	url: '',
+	method: 'POST',
+	data: { 'ciao': 'pippo', 'dd': { 'a': 'uno', 'b': 'due', 'c': 'tre' } },
+	success: function(a){ // a = xmlhttp object
 		console.log('success');
 	},
-	function(b){ // b = xmlhttp object
+	fail: function(b){ // b = xmlhttp object
 		console.log('fail');
 	}
-);
+});
